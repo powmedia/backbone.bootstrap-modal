e7a6bef7e289ebabde510683680b61406c0478aa
Jason <wwebfor@gmail.com>
2013-08-26 19:54:32 +0600

    twitter boostrap 3.0 compatibility

--- src/backbone.bootstrap-modal.js
+++ src/backbone.bootstrap-modal.js
@@ -1,6 +1,6 @@
 /**
  * Bootstrap Modal wrapper for use with Backbone.
- * 
+ *
  * Takes care of instantiation, manages multiple modals,
  * adds several options and removes the element from the DOM when closed
  *
@@ -22,6 +22,7 @@
   }
 
   var template = _.template('\
+    <div class="modal-dialog"><div class="modal-content">\
     <% if (title) { %>\
       <div class="modal-header">\
         <% if (allowCancel) { %>\
@@ -39,11 +40,12 @@
       <% } %>\
       <a href="#" class="btn ok btn-primary">{{okText}}</a>\
     </div>\
+    </div></div>\
   ');
 
   //Reset to users' template settings
   _.templateSettings = _interpolateBackup;
-  
+
 
   var Modal = Backbone.View.extend({
 
@@ -114,7 +116,7 @@
 
     /**
      * Creates the DOM element
-     * 
+     *
      * @api private
      */
     render: function() {
@@ -187,7 +189,7 @@
 
           self.trigger('cancel');
         });
-        
+
         $(document).one('keyup.dismiss.modal', function (e) {
           e.which == 27 && self.trigger('cancel');
 
@@ -207,7 +209,7 @@
       if (cb) {
         self.on('ok', cb);
       }
-      
+
       return this;
     },
 
